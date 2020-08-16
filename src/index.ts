763d56875b0e627998b626f0903dbe0914923b6e
Albert Gergo <gergoalbert.gta@gmail.com>
2020-08-16 18:33:40 +0300

    Added cors middleware and added a cron-like job scheduler

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import schedule from "node-schedule";
+import cors from "cors";
 import api from "./api/api";
 import cnnScraper from "./scrapers/cnn";
 import foxScraper from "./scrapers/fox";
@@ -6,8 +8,13 @@ import foxScraper from "./scrapers/fox";
 const app = express();
 const PORT = process.env.PORT || 5050;
 
-cnnScraper();
-foxScraper();
+const scrape = schedule.scheduleJob("0 12 * * *", () => {
+    console.log("Scrapped");
+    cnnScraper();
+    foxScraper();
+});
+
+app.use(cors());
 
 app.use("/api/v1", api);
 
